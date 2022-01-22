de4a3da8678f97484ee9d42d4f5d5c35ae6b170b
ksato-dev <sksoft.tech2020@gmail.com>
2022-01-22 19:14:38 +0900

    Added q1_1 in 19_graph.

---
+++ 19_graph/q1_1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Don't use hoge."""
+
+if __name__ == '__main__':
+    n_a_b_str = input()
+    N, A, B = map(int, n_a_b_str.split())
+    # print(N, A, B)
+
+    # 入力
+    G = [[False] * N for i in range(N)]
+    for a in range(N):
+        relationship = input()
+        for b, o_or_x in enumerate(relationship):
+            # 頂点 A から頂点 B への辺を張る
+            if o_or_x == 'o':
+                G[a][b] = True
+
+                # 無向グラフの場合は次も実施
+                G[b][a] = True
+
+    if (G[A][B] and G[B][A]):
+        print('Yes')
+    else:
+        print('No')
