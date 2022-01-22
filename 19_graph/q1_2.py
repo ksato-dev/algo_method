080e24b0e61a56469340b4a12913ae83771d3831
ksato-dev <sksoft.tech2020@gmail.com>
2022-01-22 19:30:45 +0900

    Added q1_2 in 19_graph.

---
+++ 19_graph/q1_2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Q1-2.follow."""
+
+if __name__ == '__main__':
+    n_a_b_str = input()
+    N, M = map(int, n_a_b_str.split())
+
+    # 入力
+    G = [[] for i in range(N)]
+    for _ in range(M):
+        relationship = input()
+        a, b = map(int, relationship.split())
+        # 頂点 A から頂点 B への辺を張る
+        G[a].append(b)
+
+    for a in range(N):
+        adj_nodes = G[a]
+        adj_nodes.sort()
+        out_str = ' '.join([str(b) for b in adj_nodes])
+        print(out_str)
