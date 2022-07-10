5ec02da98f4005cd9d0e089655e159ce7475682f
ksato-dev <sksoft.tech2020@gmail.com>
2022-07-10 16:21:08 +0900

    Solved.

--- 19_graph/q2_3.py
+++ 19_graph/q2_3.py
@@ -2,16 +2,18 @@ import sys
 sys.setrecursionlimit(10000)
 
 
-def dfs(graph, node_id, visited):
+def dfs(graph, node_id, visited, depth, depth_list):
     if visited[node_id]:
+        depth_list[node_id] = depth
         return
+
     visited[node_id] = True
-    print(node_id, end=" ")
 
     adj_id_list = graph[node_id]
     for adj_id in adj_id_list:
-        if adj_id >= 0:
-            dfs(graph, adj_id, visited)
+        if not visited[adj_id]:
+            depth_list[adj_id] = depth + 1
+            dfs(graph, adj_id, visited, depth + 1, depth_list)
 
 
 n = int(input())
@@ -20,10 +22,15 @@ graph = [list() for _ in range(n)]
 
 for i in range(1, n):
     parent_node_id = p_list[i - 1]
+    # 親から子へのエッジを貼れば良いらしい。
     # graph[i].append(parent_node_id)
     graph[parent_node_id].append(i)
 
 visited = [False] * n
+depth_list = [0] * n
 
-dfs(graph, 0, visited)
-print()
+depth_list[0] = 0
+dfs(graph, 0, visited, depth_list[0], depth_list)
+
+for i in range(n):
+    print(depth_list[i])
